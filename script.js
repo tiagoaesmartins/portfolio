ed33f04fc9bef5740214ec0ae720e336aa5730bd
tiagoaesmartins <tiagoaesmartins@gmail.com>
2023-10-03 19:45:33 +0100

    Finished interaction

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const darkMode = document.getElementById('dark');
-const body = document.getElementsByTagName('body');
+const btn = document.querySelector("button");
+const body = document.body;
+
+
+btn.addEventListener("click", function() {
+    body.classList.toggle("dark-mode");
+});
