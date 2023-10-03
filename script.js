bffaf40bb457b01b1931150a05958cf1d4e7748e
tiagoaesmartins <tiagoaesmartins@gmail.com>
2023-10-04 00:53:09 +0100

    Changed function in script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const btn = document.querySelector("button");
 const body = document.body;
 
+let checker = true;
+
+const toggleDark = () => {
+
+
+    if(checker == true) {
+        body.classList.add("dark-mode");
+        checker = false;
+    } else if (checker == false) {
+        body.classList.remove("dark-mode");
+        checker = true;
+    }
+}
 
 btn.addEventListener("click", function() {
-    body.classList.toggle("dark-mode");
+    toggleDark();
 });
